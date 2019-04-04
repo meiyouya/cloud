0cc520cfe623e2f1705e5c9bda4a002dced8a726
meiyouya <837184598@qq.com>
2019-04-05 01:13:26 +0800

    set cloud bus

--- cloud-parent/hi-client/src/main/java/com/zql/hi/HiClientApplication.java
+++ cloud-parent/hi-client/src/main/java/com/zql/hi/HiClientApplication.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableEurekaClient     // 表明自己是一个服务（client）
 @RestController
 @Slf4j
+@RefreshScope
 public class HiClientApplication {
 
     public static void main(String[] args) {
