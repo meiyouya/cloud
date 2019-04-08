26bb8ed65426446dc3ae4e062dcb29231cd224ca
= <=>
2019-04-08 17:23:43 +0800

    hystrix and hystrix dashboard

--- cloud-parent/hi-client/src/main/java/com/zql/hi/HiClientApplication.java
+++ cloud-parent/hi-client/src/main/java/com/zql/hi/HiClientApplication.java
@@ -1,6 +1,7 @@
 package com.zql.hi;
 
 import brave.sampler.Sampler;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.zql.hi.entity.User;
 import com.zql.hi.service.IUserService;
 import io.swagger.annotations.ApiOperation;
@@ -10,9 +11,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,20 +28,23 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableEurekaClient     // 表明自己是一个服务（client）
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 @RestController
 @Slf4j
 @RefreshScope   // 开启配置刷新
+@EnableHystrix  // 开启断路器
+@EnableHystrixDashboard     // 开启断路器监控
 public class HiClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HiClientApplication.class, args);
     }
 
-    @Value("${server.port}")
-    String port;
+//    @Value("${server.port}")
+//    String port;
 
-    @Value("${version}")
-    String version;
+//    @Value("${version}")
+//    String version;
 
     @Autowired
     private IUserService userService;
@@ -53,10 +60,16 @@ public class HiClientApplication {
 
     @GetMapping("/hi")
     @ApiOperation("根据用户id向用户问好")
+    @HystrixCommand(fallbackMethod = "hiError")
     public String home(@RequestParam(value = "id", defaultValue = "forezp") @ApiParam(name = "id",value = "用户id") Integer id) {
         User user = userService.getById(id);
         log.info("查询到的用户信息：" + user);
-        return "hi " + user.getUsername() + " ,i am from port:" + port + " this version is " + version;
+//        return "hi " + user.getUsername() + " ,i am from port:" + port + " this version is " + version;
+        return "hi " + user.getUsername();
+    }
+
+    private String hiError(Integer id) {
+        return "query error";
     }
 
     @RequestMapping("/hi")
